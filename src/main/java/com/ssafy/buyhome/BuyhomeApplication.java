50454a6622336dd9435b8318cd47165edca37382
qww1552 <qww1552@naver.com>
2022-11-17 16:18:39 +0900

    first commit

---
+++ src/main/java/com/ssafy/buyhome/BuyhomeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.buyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyhomeApplication.class, args);
+	}
+
+}
